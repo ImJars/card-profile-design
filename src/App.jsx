14af086d3624f8020fc341e883f6c27696aa2bff
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2024-09-11 23:58:20 -0600

    Update design

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,15 @@ function App() {
     <>
       <section className='h-screen w-screen bg-gradient-to-r from-bg_primary to-bg_secondary'>
         <div className='w-full h-full flex justify-center items-center'>
-          <div className='bg-bg_card w-[40%] h-[40%] rounded-2xl'>
-            <h1 className='text-text_primary'>Hello World!</h1>
+          <div className='bg-bg_card w-[40%] h-[40%] rounded-2xl flex justify-center items-center'>
+            <div className='flex space-x-8'>
+              <div>
+                <img src="profile.jpg" className='rounded-xl w-40 h-60 object-cover' alt="" />
+              </div>
+              <p className='text-center text-text_secondary'>
+                This is a React Tailwind CSS Starter Template
+              </p>
+            </div>
           </div>
         </div>
       </section>
