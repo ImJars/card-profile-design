527aa6ac9f71e7f497483259f9fe7925289eac77
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2024-09-11 23:52:08 -0600

    Update design

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <section className='h-screen w-screen bg-gradient-to-r from-bg_primary to-bg_secondary'>
+        <div className='w-full h-full flex justify-center items-center'>
+          <div className='bg-bg_card w-[40%] h-[40%] rounded-2xl'>
+            <h1 className='text-text_primary'>Hello World!</h1>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
