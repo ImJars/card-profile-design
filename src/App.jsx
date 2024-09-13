51fc38e998a012a30098d903b991d2e2aa496c3d
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2024-09-12 23:59:34 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,31 @@ function App() {
         <div className="w-full h-full flex justify-center items-center">
           <div className="bg-bg_card w-[40%] h-[40%] rounded-2xl flex justify-center items-center">
             <div className="flex space-x-8 justify-between">
-              <div>
+              <div className="flex transform -translate-x-10">
                 <img
                   src="profile.jpg"
-                  className="rounded-xl w-40 h-60 object-cover"
+                  className="rounded-xl w-52 h-72 object-cover"
                   alt=""
                 />
               </div>
-              <div className="w-2/4">
+              <div className="w-[60%]">
                 <h1 className="text-text_primary text-3xl">Angel Ramirez</h1>
                 <h2 className="text-text_secondary text-lg">
                   Software engineering
                 </h2>
-                <p className="text-text_tertiary text-base">
+                <p className="text-text_tertiary text-base mt-2">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
                   quod quae, voluptate, quos, quas quia quibusdam voluptatibus
                   sit et dolores iusto. Eaque, quod. Quisquam, quidem.
                 </p>
-                <div>
+                <div className="mt-4">
                   <div className="w-full flex space-x-6">
-                    <button className="border-text_primary border rounded-xl text-text_primary px-8 py-2">Profile</button>
-                    <button className="border-text_primary border rounded-xl bg-text_primary text-bg_card px-8 py-2">Follow</button>
+                    <button className="border-text_primary border rounded-xl text-text_primary px-8 py-2">
+                      Profile
+                    </button>
+                    <button className="border-text_primary border rounded-xl bg-text_primary text-bg_card px-8 py-2">
+                      Follow
+                    </button>
                   </div>
                 </div>
               </div>
