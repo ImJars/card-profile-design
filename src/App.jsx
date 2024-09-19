cef6e26a4778f17285c111b81f6542b2a651f3ef
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2024-09-18 22:45:05 -0600

    Add links-design-url

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
                   alt=""
                 />
               </div>
-              <div className="w-[65%] flex justify-center items-center">
+              <div className="w-[68%] flex justify-center items-center">
                 <div className="mr-10">
                   <h1 className="text-text_primary text-3xl mt-4">
                     Angel Ramirez
@@ -30,41 +30,56 @@ function App() {
                   <h2 className="text-text_secondary text-lg">
                     Software engineering
                   </h2>
-                  <p className="text-text_tertiary text-base mt-2">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quis quod quae, voluptate, quos, quas quia quibusdam
-                    voluptatibus sit et dolores iusto. Eaque, quod. Quisquam,
-                    quidem.
+                  <p className="text-text_tertiary text-base mt-2 font-semibold">
+                    With over 3 years of experience in software development, I
+                    am passionate about programming and problem-solving. I
+                    specialize in web development, mobile applications, and
+                    desktop software. I am always seeking new challenges and
+                    opportunities for improvement.
                   </p>
                   <div className="mt-4">
                     <div className="w-full flex space-x-6">
-                      <button className="border-text_primary border rounded-xl text-text_primary px-8 py-2">
-                        Profile
-                      </button>
-                      <button className="border-text_primary border rounded-xl bg-text_primary text-bg_card px-8 py-2">
-                        Follow
-                      </button>
+                      <a
+                        className="border-text_primary border rounded-xl text-text_primary px-8 py-2 hover:bg-text_primary hover:text-bg_card transition-all duration-300 ease-in-out"
+                        href="https://jarsdev.netlify.app/blog"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <button>Profile</button>
+                      </a>
+                      <a
+                        className="border-text_primary border rounded-xl bg-text_primary text-bg_card px-8 py-2 hover:bg-bg_card hover:text-text_primary transition-all duration-300 ease-in-out"
+                        href="https://jarsdev.netlify.app/blog"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <button>Follow</button>
+                      </a>
                     </div>
                   </div>
                   <div className="mt-6">
                     <div className="flex justify-end">
                       <ul className="text-text_secondary flex space-x-4">
-                        <Icon>
+                        <Icon link={"https://jarsdev.netlify.app/"}>
                           <TbWorldCode />
                         </Icon>
-                        <Icon>
+                        <Icon link={"https://github.com/ImJars"}>
                           <FaGithub />
                         </Icon>
-                        <Icon>
+                        <Icon link={"https://www.linkedin.com/in/imjars/"}>
                           <FaLinkedin />
                         </Icon>
-                        <Icon>
+                        <Icon link={"https://x.com/Im_Jars"}>
                           <BsTwitterX />
                         </Icon>
-                        <Icon>
+                        <Icon link={"https://www.instagram.com/jars.little/"}>
                           <FaInstagram />
                         </Icon>
-                        <Icon>
+                        <Icon
+                          link={
+                            "https://www.facebook.com/joseangel.ramirezsanchez.92/?locale=es_LA"
+                          }
+                        >
                           <FaFacebookSquare />
                         </Icon>
                       </ul>
--- src/assets/icon.jsx
+++ src/assets/icon.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from "react";
 
-function Icon({ children }) {
+function Icon({ children, link }) {
   return (
     <>
-        <li className='hover:scale-125 transition-all duration-300 ease-in-out'>
-            <a href="#" className='text-lg '>
-                {children}
-            </a>
-        </li>
+      <li className="hover:scale-125 transition-all duration-300 ease-in-out">
+        <a 
+          href={link} 
+          className="text-lg"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {children}
+        </a>
+      </li>
     </>
-  )
+  );
 }
 
-export default Icon
+export default Icon;
